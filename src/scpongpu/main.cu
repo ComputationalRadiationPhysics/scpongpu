5ccd9b64426cb58ee4966f2c4840f2e5c493cc67
PrometheusPi <r.pausch@hzdr.de>
2014-09-14 02:58:36 +0200

    add comments to main.cu

--- src/scpongpu/main.cu
+++ src/scpongpu/main.cu
@@ -1,74 +1,85 @@
 #include "plasma.hpp"
 
 
-
-
-
 int main(void)
 {
-  // set parameters:
+  /* parameters are in namespace parameters */
   using namespace parameters;
 
-  // ############################################################
+  /*############################################################*/
+  /*               prepare simulation                           */
+  /*############################################################*/
 
+  /* verbose output of particles and parallelization */
   std::cout << "Number of particles: " << N_particle << std::endl;
   std::cout << "blocksize:           " << blocksize << std::endl;
 
-  // set up start parameters and copy them to GPU:
-  vec location_h[N_particle];   // particle loaction
-  vec speed_h[N_particle];      // particle speed
+  /* set up start parameters and copy them to GPU:*/
+  vec location_h[N_particle];   /* particle location on host */
+  vec speed_h[N_particle];      /* particle speed on host (default=0) */
 
-  Uniformly<numtype> uni(-1.0e-5f, 1.0e-5f);  // random distribution for positioning
+  /* TODO replace magic numbers */
+  Uniformly<numtype> uni(-1.0e-5f, 1.0e-5f);  /* random distribution for positioning */
   for(unsigned int i=0; i<N_particle; ++i)
     {
+      /* give each particle a random position */
       location_h[i].x = uni.get() * chi_length;
       location_h[i].y = uni.get() * chi_length;
       location_h[i].z = uni.get() * chi_length;
-      //speed_h[i].x = uni.get() * chi_length/chi_time;
-      //speed_h[i].y = uni.get() * chi_length/chi_time;
-      //speed_h[i].z = uni.get() * chi_length/chi_time;
     }
 
-  // print iteration starting values:
+  /* print iteration starting values: */
   std::cout << " Start values of finding the ground state: " << std::endl; 
-  //print_particles(location_h, speed_h, N_particle);
+  print_particles(location_h, speed_h, N_particle);
 
-  // prepare GPU part
-  unsigned int size = sizeof(vec) * N_particle;
-  vec *location_d;
+  /* prepare GPU part: */
+  unsigned int size = sizeof(vec) * N_particle; /* size of one copy of all particles */
+
+  /* device pointers: */
+  vec *location_d; 
   vec *speed_d;
   vec *accel_d;
+
+  /* allocate and copy position, speed, acceleration */
   cudaMalloc((void**)&location_d, size);
   cudaMalloc((void**)&speed_d,    size);
   cudaMalloc((void**)&accel_d,    size);
   cudaMemcpy(location_d, location_h, size, cudaMemcpyHostToDevice);
   cudaMemcpy(speed_d,    speed_h,    size, cudaMemcpyHostToDevice);
  
+  /* set parallelization parameters: */
   dim3 gridDim(N_particle/blocksize, 1, 1);
   dim3 blockDim(blocksize, 1, 1);
 
 
+  /* ################################################################ */
+  /*                     run simulation                               */
+  /* ################################################################ */
 
-
-  // ################################################################
-
-  // run simulation:
+  /* find physically correct start position */
   finding_ground_state(location_h, speed_h, location_d, speed_d, accel_d, size, gridDim, blockDim);
+
+  /* overwrite particle speed to set predefined temperature */
   heating(location_h, speed_h, speed_d, size);
+
+  /* simulate laser cooling */
   cool_down(location_d, speed_d, accel_d, gridDim, blockDim);
 
-  // #################################################################
 
 
 
 
-  // copy results to CPU:
+  /* ################################################################# */
+  /*                finish simulation                                  */
+  /* ################################################################# */
+
+  /* copy results from GPU to CPU: */
   cudaMemcpy(location_h, location_d, size, cudaMemcpyDeviceToHost);
   cudaMemcpy(speed_h,    speed_d,    size, cudaMemcpyDeviceToHost);
   cudaFree(location_d);
   cudaFree(speed_d);
 
-  // print results:
+  /* print results: */
   std::cout << std::endl << std::endl << "final values: " << std::endl;
   print_particles(location_h, speed_h, N_particle);
 
