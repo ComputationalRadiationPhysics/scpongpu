f1e42732687dddd9e3172e3f9b463881699e96df
PrometheusPi <r.pausch@hzdr.de>
2014-09-29 23:46:19 +0200

    avoid using namespace parameters use pythonic parameters as param

--- src/scpongpu/main.cu
+++ src/scpongpu/main.cu
@@ -24,29 +24,29 @@
 int main(void)
 {
   /* parameters are in namespace parameters */
-  using namespace parameters;
+  namespace param = parameters;
 
   /*############################################################*/
   /*               prepare simulation                           */
   /*############################################################*/
 
   /* verbose output of particles and parallelization */
-  std::cout << "Number of particles: " << N_particle << std::endl;
-  std::cout << "blocksize:           " << blocksize << std::endl;
+  std::cout << "Number of particles: " << param::N_particle << std::endl;
+  std::cout << "blocksize:           " << param::blocksize << std::endl;
 
   /* set up start parameters and copy them to GPU:*/
-  vec location_h[N_particle];   /* particle location on host */
-  vec speed_h[N_particle];      /* particle speed on host*/
-  vec accel_h[N_particle];      /* particle acceleration on host*/
+  vec location_h[param::N_particle];   /* particle location on host */
+  vec speed_h[param::N_particle];      /* particle speed on host*/
+  vec accel_h[param::N_particle];      /* particle acceleration on host*/
 
   /* TODO replace magic numbers */
   Uniformly<numtype, SeedSelected> uni(-1.0e-5f, 1.0e-5f);  /* random distribution for positioning */
-  for(unsigned int i=0; i<N_particle; ++i)
+  for(unsigned int i=0; i<param::N_particle; ++i)
     {
       /* give each particle a random position */
-      location_h[i].x = uni.get() * chi_length;
-      location_h[i].y = uni.get() * chi_length;
-      location_h[i].z = uni.get() * chi_length;
+      location_h[i].x = uni.get() * param::chi_length;
+      location_h[i].y = uni.get() * param::chi_length;
+      location_h[i].z = uni.get() * param::chi_length;
       
       /* give each particle a zeros speed and zero acceleration */      
       speed_h[i].zero();
@@ -56,10 +56,10 @@ int main(void)
 
   /* print iteration starting values: */
   std::cout << " Start values of finding the ground state: " << std::endl; 
-  print_particles(location_h, speed_h, N_particle);
+  print_particles(location_h, speed_h, param::N_particle);
 
   /* prepare GPU part: */
-  unsigned int size = sizeof(vec) * N_particle; /* size of one copy of all particles */
+  unsigned int size = sizeof(vec) * param::N_particle; /* size of one copy of all particles */
 
   /* device pointers: */
   vec *location_d; 
@@ -75,8 +75,8 @@ int main(void)
   cudaMemcpy(accel_d,    accel_h,    size, cudaMemcpyHostToDevice);
  
   /* set parallelization parameters: */
-  dim3 gridDim(N_particle/blocksize, 1, 1);
-  dim3 blockDim(blocksize, 1, 1);
+  dim3 gridDim(param::N_particle/param::blocksize, 1, 1);
+  dim3 blockDim(param::blocksize, 1, 1);
 
 
   /* ################################################################ */
@@ -93,7 +93,7 @@ int main(void)
   /* print starting values: */
   /* verbose output */
   std::cout << "Start values: " << std::endl; 
-  print_particles(location_h, speed_h, N_particle);
+  print_particles(location_h, speed_h, param::N_particle);
 
   /* simulate laser cooling */
   cool_down(location_d, speed_d, accel_d, gridDim, blockDim);
@@ -116,7 +116,7 @@ int main(void)
 
   /* print results: */
   std::cout << std::endl << std::endl << "final values: " << std::endl;
-  print_particles(location_h, speed_h, N_particle);
+  print_particles(location_h, speed_h, param::N_particle);
 
   return 0;
 }
